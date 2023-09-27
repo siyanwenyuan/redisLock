96f253b15105714d9f9690b35d2592d46f6b193b
chenwan <3335018910@qq.com>
2023-09-27 10:47:37 +0800

    创建github版本的redis分布式锁的基础代码结构

---
+++ src/main/java/com/chen/redislockgithub/RedisLockGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.chen.redislockgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLockGitHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisLockGitHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chen/redislockgithub/RedisLockGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.redislockgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLockGitHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
